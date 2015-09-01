dfec7889dd3e271a80b0a156630ad41de24f112f
Juan <jvaron@princeton.edu>
2015-08-31 21:01:30 -0400

    App Update

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(8080, function(){
-  console.log('listening on *:3000');
+http.listen(process.env.PORT || 5000, function(){
+  console.log('listening');
 });
