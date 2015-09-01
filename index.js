19f831ea3b35d5d3aede42787f311e7a2d582444
Juan <jvaron@princeton.edu>
2015-09-01 02:06:04 -0400

    Update

--- index.js
+++ index.js
@@ -2,17 +2,19 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var twilio = require('twilio');
+var bodyParser = require('body-parser');
 var client = new twilio.RestClient('AC1778dd02a7617de146d209cbea72b9a4', '6b5698ad3a3b6340fccf563cf1824566');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-
 app.post('/twiml', function(req, res) {
         var twiml = new twilio.TwimlResponse();
 
-        twiml.say('Hi!  Thanks for checking out my app!');
+        twiml.message('Hi!  Thanks for checking out my app!');
 
         res.type('text/xml');
         res.send(twiml.toString());
@@ -21,7 +23,7 @@ app.post('/twiml', function(req, res) {
 app.get('/twiml', function(req, res) {
         var twiml = new twilio.TwimlResponse();
 
-        twiml.say('Hi!  Thanks for checking out my app!');
+        twiml.message('Hi!  Thanks for checking out my app!');
 
         res.type('text/xml');
         res.send(twiml.toString());
