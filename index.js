3ff77a15e18c3e6c0370b44fc1d4c350e35fa38b
Juan <jvaron@princeton.edu>
2015-09-01 03:14:21 -0400

    Update

--- index.js
+++ index.js
@@ -7,12 +7,24 @@ var client = new twilio.RestClient('AC1778dd02a7617de146d209cbea72b9a4', '6b5698
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
+
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
 app.post('/twiml', function(req, res) {
-        var twiml = new twilio.TwimlResponse();
+        if (twilio.validateExpressRequest(req, '6b5698ad3a3b6340fccf563cf1824566')) {
+	        var resp = new twilio.TwimlResponse();
+	        resp.say('express sez - hello twilio!');
+
+	        res.type('text/xml');
+	        res.send(resp.toString());
+	    }
+	    else {
+	        res.send('you are not twilio.  Buzz off.');
+	    }
+
+        /*var twiml = new twilio.TwimlResponse();
         //res.setHeader('Access-Control-Allow-Origin', '*');
         //res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
         //res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
@@ -21,7 +33,7 @@ app.post('/twiml', function(req, res) {
         twiml.message('Hi!  Thanks for checking out my app!');
 
         res.type('text/xml');
-        res.send(twiml.toString());
+        res.send(twiml.toString());*/
 });
 app.get('/twiml', function(req, res) {
         var twiml = new twilio.TwimlResponse();
