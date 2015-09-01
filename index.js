e9fb9fecd084199e97f5c3aa5f4a39345203463b
Juan <jvaron@princeton.edu>
2015-08-31 21:39:56 -0400

    Linked Twilio
    Added Responses to text messages

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var client = require('twilio')('AC2b5e8aa4e79fa1cb79bc1fed04c5a926', '253d025879c7aa207751243448bf8f4c');
+
+app.configure(function () {
+    /* Configure your express app... */
+    app.use(express.urlencoded());
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
+});
+
+app.post('/twiml', function(req, res) {
+    if (twilio.validateExpressRequest(req, '253d025879c7aa207751243448bf8f4c')) {
+        var resp = new twilio.TwimlResponse();
+        resp.say('express sez - hello twilio!');
+
+        res.type('text/xml');
+        res.send(resp.toString());
+    }
+    else {
+        res.send('you are not twilio.  Buzz off.');
+    }
 });
 
 io.on('connection', function(socket){
